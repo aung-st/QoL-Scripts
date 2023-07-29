8db1f04ebb1d564e74761e71a11be03b702674e9
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-07-29 11:21:01 +0100

    added skeleton methods

---
+++ Expiry-Check/expiry.py
@@ -0,0 +1,16 @@
+import datetime 
+class expiry():
+    def __init__(self) -> None:
+        pass
+
+    def add(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def display(self):
+        pass
+
+if __name__ == "__main__":
+    pass
